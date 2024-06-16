b3480dba264efdd19720b49d193a89b504568a34
Benjamin Harris <113062337+hbjamin@users.noreply.github.com>
2024-06-16 20:15:31 +0200

    Create variables.js

---
+++ variables.js
@@ -0,0 +1,21 @@
+let game = [];
+let over = false;
+
+let answer = [];
+let lettersFound = [];
+
+let keyboard = [];
+row1 = ['Q','W','E','R','T','Y','U','I','O','P'];
+row2 = ['A','S','D','F','G','H','J','K','L'];
+row3 = ['Z','X','C','V','B','N','M'];
+
+let tries = 0;
+let index = 0;
+
+let canvasX = 300;
+let canvasY = 500;
+
+let myGreen = [0,150,50];
+let myYellow = [200,200,0];
+let myGray = [100,100,100];
+let myLGray = [220,220,220];
